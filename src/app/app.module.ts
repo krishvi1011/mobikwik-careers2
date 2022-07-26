f5a8f9a4776c0369f5800e103bda0273af642cb4
khushangdhingra <khushangd@gmail.com>
2022-07-26 18:31:49 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './app/nav-bar/nav-bar.component';
+import { MainContentComponent } from './app/main-content/main-content.component';
+import { ImageSliderComponent } from './app/image-slider/image-slider.component';
+import { RolesComponent } from './app/roles/roles.component';
+import { RoleComponent } from './app/roles/role/role.component';
+import { AddressComponent } from './app/address/address.component';
+import { FooterComponent } from './app/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainContentComponent,
+    ImageSliderComponent,
+    RolesComponent,
+    RoleComponent,
+    AddressComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
